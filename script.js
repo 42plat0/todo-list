ba0644176e5806938cec15151b1a7f1b48717892
42plat0 <gytisnaujasis@icloud.com>
2024-04-07 16:29:58 +0300

    add functioning edit and done buttons; add text area to edit task name; add classes for input text area and focus/focusout event listeners

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //make into class
 let textInput = document.querySelector("#input-text")
-let addTaskBtn = document.querySelector("#add-btn")
 
+let addTaskBtn = document.querySelector("#add-btn")
+let body = document.querySelector("body")
 let editTaskBtn, deleteTaskBtn;
 
 let taskBar = document.querySelector("#task-bar");
@@ -11,32 +12,81 @@ window.addEventListener("load", ()=>{
 //input text
 //add text as task name
 addTaskBtn.addEventListener("click", ()=>{
+    console.log(textInput.value)
     let taskName = textInput.value;
-    createTask(taskName);
+    if(taskName){
+        createTask(taskName);
+        textInput.value = "Write a task here"
+    }
     let taskList = Array.from(taskBar.children)
 
-    taskList.forEach((e)=>{
-        let task = Array.from(e.children);
+    taskList.forEach((taskBlock)=>{
+        let task = Array.from(taskBlock.children);
         task.forEach((element) =>{
             if (element.id === "done-task"){
-                element.addEventListener("click", (e) =>{
-                    console.log(e)
-                    console.log(taskBar)
+                element.addEventListener("click", () =>{
+                    taskBar.removeChild(taskBlock)
                 })
             }
             else if (element.id === "edit-task"){
                 element.addEventListener("click", (e) =>{
-                    console.log("edit")
+                    let taskTitle = taskBlock.children[0].innerHTML;
+                    //create text edit panel and append to concrete task
+                    if (taskBlock.children.length < 4){
+                        createTextEdit(taskTitle, taskBlock)
+                    }
+
+                    let saveTaskTitle = document.querySelector("#text-change-btn")
+                    let editedTaskTitle = document.querySelector("#text-change-area")
+                    
+                    saveTaskTitle.addEventListener("click", () =>{
+                        if(editedTaskTitle.value){
+                            //task name change if
+                            taskBlock.children[0].innerHTML = editedTaskTitle.value;
+                        }
+                        //after saving we remove option to change name again
+                        saveTaskTitle.remove()
+                        editedTaskTitle.remove()
+                    })
+                    //removes text when preparing to enter
+                    editedTaskTitle.addEventListener("focus", ()=>{
+                        editedTaskTitle.value = ""
+                    })
+
                 })
             }
         })
     })
-    
-    
+})
 
+textInput.addEventListener("focus", ()=>{
+    textInput.className = "input-txt";
+    textInput.value = ""
+})
+
+textInput.addEventListener("focusout", ()=>{
+    textInput.className = "temporary-text";
 })
 
 
+function createTextEdit(textToEdit, taskBox){
+    let container = document.createElement("div");
+    container.id = "text-change-container"
+
+    let textArea = document.createElement("textarea");
+    textArea.id = "text-change-area"
+    textArea.innerHTML = textToEdit;
+
+
+    let btn = document.createElement("button");
+    btn.id = "text-change-btn"
+
+    btn.innerHTML = "Save"
+
+    //needed as argument to refer to it outside function
+    taskBox.append(textArea);
+    taskBox.append(btn);
+}
 
 //let edit text name
 //let delete with done
